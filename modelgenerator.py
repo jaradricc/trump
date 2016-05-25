507fd7171fb629040b0d2339c8c9818eb31307ec
David Ricardo Jaramillo Baranda <jaradricc@gmail.com>
2016-05-24 22:18:55 -0500

    Version: 0.55
    Description:
    Se cambió la forma en qu ese cuentan los retweets para solucionar problemas con pyspark usando HiveContext

--- modelgenerator.py
+++ modelgenerator.py
@@ -1,20 +1,28 @@
+from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import TfidfVectorizer
 from itertools import izip
 import numpy as np
 import operator
+import json
 
-arch = open('Downloads/tweets.csv','rb')
-d = list()
-header = arch.readline()
-for l in arch:
-    if l != header:
-        d.append(l)
+my_stop_words = text.ENGLISH_STOP_WORDS.union(my_words)
+with open('../Downloads/2016523tweets.json') as f:
+    data = json.load(f)
+
+# data contiene un diccionario con una sola llava 'tweet' que contiene otro diccionario.
+
 
 # vectorizer = TfidfVectorizer(min_df=1)
-vectorizer = TfidfVectorizer(token_pattern=u'[\S]+',min_df=1)
-tfidf_matrix = vectorizer.fit_transform(d)
+# Definimos la forma en que parte los strings para procesarlos en tfidf
+my_stop_words = text.ENGLISH_STOP_WORDS
+vectorizer = TfidfVectorizer(token_pattern=u'[\S]+',min_df=1,stop_words=set(my_stop_words))  #quitamos stopwords porque si aparecen como palabras relevantes
+# Aplica tfidf a los datos cargados del archivo (texto de los tweets)
+tfidf_matrix = vectorizer.fit_transform(data['tweet'].values())
+# Extraemos los feature_names (en nuestro caso, los features son las palabras del vocabulario de los tweets)
 feature_names = vectorizer.get_feature_names()
+# tfidf_matrix contiene la matriz de resultado tfidf, pero es numérica, por lo que es conveniente convertirla a numpy.array para procesar más rápido.
 npm = np.array(tfidf_matrix.toarray())
+# Hacemos la compisición por suma (suma de los documentos por palabra), cuyo resultado es un vector con el peso de cada palabra (ordenado de acuerdo con el arreglo de feature_names)
 wp = map(lambda i: sum(npm[:,i]), range(len(npm)))
 tfidf_dict = dict(izip(iter(feature_names),iter(wp)))
 sorted_tfidf = sorted(tfidf_dict.items(), key=operator.itemgetter(1))
--- pipeline/count_bydate.py
+++ pipeline/count_bydate.py
@@ -23,7 +23,7 @@ import numpy as np
 from pyspark.sql.types import *
 import calendar
 
-
+import json
 
 if __name__ == "__main__":
     print ("Iniciada la tarea de spark")
@@ -33,46 +33,29 @@ if __name__ == "__main__":
     sqlCtx.read.json(sys.argv[1])\
                     .registerTempTable('tweets')
 
-    def clean_date(date):
-        month_num ={v: "%02d" % (k,) for k,v in enumerate(calendar.month_abbr)}
-        day =  date.split(' ')[2]
-        month = month_num[date.split(' ')[1]]
-        year = date.split(' ')[5]
-        new_date = month + '/' + day + '/' + year
-        return new_date
 
-    sqlCtx.registerFunction('clean_date', clean_date, StringType())
+    test = sqlCtx.sql('select retweeted as is_rt, created_at as date from tweets').collect()
 
-    def create_ReTweet(text):
-        if ('RT' in text):
-            retweet = 1
+
+
+    res = dict()
+    for i in test:
+        aux = i.date.split()
+        fecha = aux[1] + '/' + aux[2] + '/' + aux[5]
+        if fecha not in res:
+            if i.is_rt:
+                res[fecha] = {'tweets':0,'retweets':1}
+            else:
+                res[fecha] = {'tweets':1,'retweets':0}
         else:
-            retweet = 0
-        return retweet
+            if i.is_rt:
+                res[fecha]['retweets'] += 1
+            else:
+                res[fecha]['tweets'] += 1
 
-    def create_Tweet(text):
-        if ('RT' not in text):
-            tweet = 1
-        else:
-            tweet = 0
-        return tweet
 
-    sqlCtx.registerFunction('create_ReTweet', create_ReTweet, IntegerType())
-    sqlCtx.registerFunction('create_Tweet', create_Tweet, IntegerType())
-
-    fechas3 = sqlCtx.sql('select  clean_date(created_at)  as created_at,\
-                text,\
-                create_ReTweet(text) as Retweet, \
-                create_Tweet(text) as Tweet \
-                from tweets')
-
-    fechas3.registerTempTable('fechas3')
-
-    sqlCtx.sql('select  created_at,  sum(Retweet) as Retweets, sum(Tweet) as Tweets\
-        from fechas3\
-        group by created_at ')\
-        .toPandas()\
-        .to_json(sys.argv[2])
+    with open(sys.argv[2],'w') as f:
+        json.dump(res,f)
 
     print ("Terminada la tarea de spark")
     sc.stop()
